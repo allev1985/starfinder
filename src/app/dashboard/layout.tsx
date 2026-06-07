5d491630ab0f33fd70a2bad1c6f6229cd2fe2914
allev1985 <allev1985@gmail.com>
2026-06-08 09:36:56 +1000

    layout scroll fixes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,11 +16,11 @@ export default async function DashboardLayout({
 
   return (
     <AppBarProvider>
-      <div className="flex h-screen flex-col" style={{ backgroundColor: "var(--bg)" }}>
+      <div className="flex h-dvh flex-col" style={{ backgroundColor: "var(--bg)" }}>
         <TopBar isAdmin={admin} />
         <AppBar />
         <div className="shrink-0 h-[56px] md:h-[54px]" aria-hidden="true" />
-        <main className="flex flex-1 min-h-0 flex-col overflow-y-auto pb-[68px] md:pb-0">
+        <main className="flex flex-1 min-h-0 flex-col overflow-y-auto overscroll-y-contain pb-[68px] md:pb-0">
           {children}
         </main>
         <TabBar />
